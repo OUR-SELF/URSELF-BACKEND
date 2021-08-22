1e8e7d6fd133a06fef4f5e2c41fa7029e1818751
white-jang <galaxy__12@naver.com>
2021-08-22 18:43:56 +0900

    media 폴더 업로드 & default img 처리

--- projects/models.py
+++ projects/models.py
@@ -35,7 +35,7 @@ class Project(models.Model):
   details = models.CharField(max_length=3000)
   counted_user_num = models.IntegerField(default=0)
 
-  thumbnail_image = models.ImageField(blank=True, null=True)
+  thumbnail_image = models.ImageField(default="default-img.png")
   detail_image = models.ImageField(blank=True, null=True)
 
 
